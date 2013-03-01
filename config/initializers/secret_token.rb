a909d1b25d8607228326b6a9a26baea3801f1e16
Jonathan Castillo <jonathan@ygm.com>
2013-03-01 12:20:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieReviewsCastillo::Application.config.secret_token = '73e1b60462bbeb392dc8402847b25e746558704b88f96da3cc3721f5603ecebe44cb15336341f7619e05a0423b8ca504401560d2d7412e1a398da72b94b856f6'
