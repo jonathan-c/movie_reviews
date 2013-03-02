07b061a3a39d56ae3c8a23bebbf4e888d86e850e
Jonathan Castillo <jonathan@ygm.com>
2013-03-01 20:23:44 -0500

    added fb profile image and email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   private
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id].nil?
+      @current_user = nil
+    else
+      @current_user ||= User.find(session[:user_id])
+    end
   end
 end
