d5e3f9e2757a8012dec7fc651d2de8ae1715fd99
Jonathan Castillo <jonathan@ygm.com>
2013-03-04 11:30:55 -0500

    Added rottwn and fresh methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,13 @@ class Movie < ActiveRecord::Base
   attr_accessible :mpaa_rating, :poster, :runtime, :synopsis, :title, :year
   
   has_many :comments
+  
+  def rotten?
+    self.critics_score > 60 ? false : true
+  end
+  
+  def fresh?
+    self.critics_score > 60 ? true : false
+  end
 
 end
